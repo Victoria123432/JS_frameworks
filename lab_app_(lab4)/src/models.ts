cb5525c5de43588c596451e6e804e17ef4936389
Viktoria <viktoriyadem123456789@gmail.com>
2024-09-24 02:13:50 +0300

    24.09

--- lab_app_(lab4)/src/models.ts
+++ lab_app_(lab4)/src/models.ts
@@ -1,65 +1,74 @@
 export interface IBook {
-    title: string;
-    author: string;
-    year: number;
+  title: string;
+  author: string;
+  year: number;
 
-    getDetails(): string;
+  getDetails(): string;
 }
 
-export interface IUser{
-    name: string;
-    email: string;
+export interface IUser {
+  name: string;
+  email: string;
 
-    getInfo(): string;
+  getInfo(): string;
 }
 
-export class Book implements IBook{
-    title: string;
-    author: string;
-    year: number;
-    isBorrowed: boolean;
-    borrowedBy?: number;
-    constructor(title: string, author: string, year: number, isBorrowed: boolean = false){
-     this.title = title;
-     this.author = author;
-     this.year = year;
-     this.isBorrowed = isBorrowed;
-    }
+export class Book implements IBook {
+  title: string;
+  author: string;
+  year: number;
+  isBorrowed: boolean;
+  borrowedBy?: number;
+  constructor(
+    title: string,
+    author: string,
+    year: number,
+    isBorrowed: boolean = false,
+  ) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.isBorrowed = isBorrowed;
+  }
 
-    getDetails(): string {
-        return `${this.title} написана ${this.author} у ${this.year} році`
-    }
+  getDetails(): string {
+    return `${this.title} написана ${this.author} у ${this.year} році`;
+  }
 }
 
-export class User implements IUser{
-    name: string;
-    email: string;
-    borrowedBookCount: number;
-    borrowedBooks: Book[];
+export class User implements IUser {
+  name: string;
+  email: string;
+  borrowedBookCount: number;
+  borrowedBooks: Book[];
 
-    constructor(name: string, email: string, borrowedBookCount: number = 0, borrowedBooks: Book[] = []){
-        this.name =name;
-        this.email = email;
-        this.borrowedBookCount = borrowedBookCount;
-        this.borrowedBooks = borrowedBooks;
-    }   
+  constructor(
+    name: string,
+    email: string,
+    borrowedBookCount: number = 0,
+    borrowedBooks: Book[] = [],
+  ) {
+    this.name = name;
+    this.email = email;
+    this.borrowedBookCount = borrowedBookCount;
+    this.borrowedBooks = borrowedBooks;
+  }
 
-    getInfo(): string{
-        return `Name: ${this.name}, Borrowed Books: ${this.borrowedBooks.length}`;
-    }
+  getInfo(): string {
+    return `Name: ${this.name}, Borrowed Books: ${this.borrowedBooks.length}`;
+  }
 
-    canBorrowMoreBooks(): boolean{
-        return this.borrowedBookCount < 3
-    }
+  canBorrowMoreBooks(): boolean {
+    return this.borrowedBookCount < 3;
+  }
 
-    borrowBook(book: Book): void{
-        this.borrowedBookCount++;
-        this.borrowedBooks.push(book);
-    }
+  borrowBook(book: Book): void {
+    this.borrowedBookCount++;
+    this.borrowedBooks.push(book);
+  }
 
-    returnBook(book: Book){
-        this.borrowedBookCount--;
-        this.borrowedBooks = this.borrowedBooks.filter(b => b !== book);
-    }
-
+  returnBook(book: Book) {
+    this.borrowedBookCount--;
+    this.borrowedBooks = this.borrowedBooks.filter((b) => b !== book);
+  }
 }

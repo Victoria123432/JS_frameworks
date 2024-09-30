c6aa054946947e57cf4f58bbaadf95c9bb70339f
Viktoria <viktoriyadem123456789@gmail.com>
2024-10-01 01:37:13 +0300

    01.10

--- lab_app_(lab4)/eslint.config.mjs
+++ lab_app_(lab4)/eslint.config.mjs
@@ -11,8 +11,8 @@ export default [
       prettier,
     },
     rules: {
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-      "@typescript-eslint/no-explicit-any": "off",
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      '@typescript-eslint/no-explicit-any': 'off',
     },
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
